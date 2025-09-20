db54672a992cb79446e2450809e24c6755cb1275
piecekins <joshuagrushton@gmail.com>
2025-09-19 19:34:05 -0500

    first 2 of the 3 prove week 2

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,60 @@
 using System;
+using System.Data;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool guessed = false;
+        int trys = 0;
+        //Console.WriteLine("Hello Prep3 World!");
+
+        //Console.Write("What is the magic number? ");
+        //int magicNumber = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        // After looking at the Sample Solution I changed this line of code from (1, 100) to (1, 101) as I didn't relize it had to be 101 to have it 1 through 100 its the same thing with line 49 as well. 
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        while (guessed == false)
+        {
+            trys += 1;
+            Console.Write("What is your guess? ");
+            int guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else
+            {
+                Console.WriteLine("You Guessed it!!!");
+                Console.WriteLine($"You took {trys} guesses to win!");
+
+                Console.WriteLine("Do you want to play again?");
+                string answer = Console.ReadLine();
+
+                if (answer == "yes" || answer == "Yes")
+                {
+                    guessed = false;
+                    trys = 0;
+                    randomGenerator = new Random();
+                    magicNumber = randomGenerator.Next(1, 101);
+                }
+                else
+                {
+                    guessed = true;
+                }
+            }
+
+
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Numerics;
+using System.Security.Principal;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //Console.WriteLine("Hello Prep4 World!");
+        int number = -1;
+        List<int> numbers = new List<int>();
+        int total = 0;
+        int average = 0;
+        int largest = 0;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            numbers.Add(number);
+        }
+        foreach (int item in numbers)
+        {
+            total += item;
+
+            if (largest < item)
+            {
+                largest = item;
+            }
+
+        }
+        average = total / (numbers.Count - 1);
+
+        Console.WriteLine($"Sum: {total}");
+        Console.WriteLine($"The Average: {average}");
+        Console.WriteLine($"largest: {largest}");
     }
 }
