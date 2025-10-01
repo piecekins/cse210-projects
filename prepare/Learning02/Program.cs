ef9d74cdf7cf7b43a5879c74b1a9f94793db9501
piecekins <joshuagrushton@gmail.com>
2025-09-30 20:07:11 -0500

    Activity 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        //Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+
+        //Console.WriteLine(job1._jobTitle);
+
+        Job job2 = new Job();
+
+
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        job2._company = "Apple";
+        job2._startYear = 2023;
+        job2._endYear = 2023;
+        job2._jobTitle = "Manager";
+
+        //Console.WriteLine(job1._company);
+        //Console.WriteLine(job2._company);
+        //job1.DisplayJob();
+        //job2.DisplayJob();
+
+        Resume myResume = new Resume();
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume._name = "Allison Rose";
+
+        //Console.WriteLine(myResume._jobs[0]._jobTitle);
+        myResume.DisplayResume();
     }
+
+
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,23 @@
+public class Resume
+{
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+
+    public void DisplayResume()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+        //_jobs[0].DisplayJob();
+        //_jobs[1].DisplayJob();
+        // before the sample solution
+        // I should have used a foreach statement as it would let me add as many jobs as I had
+        
+
+        //Made after viewing the sample solution
+        foreach (Job job in _jobs)
+        {
+            job.DisplayJob();
+        }
+        //
+    }
+}
