faa67dea4df66a381b2ad03398f5d197f5669cf0
piecekins <joshuagrushton@gmail.com>
2025-11-13 10:38:33 -0600

    prepare

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        //Console.WriteLine("Hello Learning05 World!");
+
+        Square s = new Square("red", 5);
+        Rectangle r = new Rectangle("Green", 5, 3);
+        Circle c = new Circle("Blue", 5);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(s);
+        shapes.Add(r);
+        shapes.Add(c);
+
+        foreach (Shape ss in shapes)
+        {
+            Console.WriteLine(ss.GetColor());
+            Console.WriteLine(ss.GetArea());
+        }
+        
+
+
     }
 }
---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,30 @@
+using System.Runtime.InteropServices;
+
+public class  Shape
+{
+    protected string _color;
+
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    public virtual double GetArea()
+    {
+        return -1;
+    }
+
+
+
+}
